e2118e95cd1f258745d61c7c5053ea5a173185af
dpurcell1 <dpsvr3@gmail.com>
2021-01-12 12:13:52 -0500

    Added comments

--- wordcount.py
+++ wordcount.py
@@ -37,16 +37,26 @@ import sys
 
 def create_word_dict(filename):
     """Returns a word/count dict for the given file."""
+    # empty dictionary variable
     word_dict = {}
+    # opens file in read-only mode
     with open(filename, 'r') as f:
+        # scans each line in file
         for line in f:
+            # sets each line to lowercase
             lowercase_string = line.lower()
+            # converts each line into list of words
             word_list = lowercase_string.split()
+            # loops through word_list
             for word in word_list:
+                # conditional to check whether word exists as a key in dict
                 if word not in word_dict.keys():
+                    # add word as a key to word_dict with a value of 1
                     word_dict[word] = 1
                 else:
+                    # increment value of current word key by 1
                     word_dict[word] += 1
+    # return populated word_dict
     return word_dict
 
 
@@ -54,19 +64,25 @@ def print_words(filename):
     """Prints one per line '<word> : <count>', sorted
     by word for the given file.
     """
+    # creates word dict from given file
     word_dict = create_word_dict(filename)
-
+    # creates list of keys from word_dict
     dict_keys = list(word_dict.keys())
+    # sorts dict_keys with case insensitivity (ex. 'AAA', 'aaa', 'BBB', etc.)
     dict_keys.sort(key=str.lower)
-
+    # loops through dict_keys printing key/value pairs to console
     for i in dict_keys:
         print(i, ' : ', word_dict[i])
 
 
 def print_top(filename):
     """Prints the top count listing for the given file."""
+    # creates word dict from given file
     word_dict = create_word_dict(filename)
+    # creates list of (word, count) tuples
+    # sorted by last value [-1] of every tuple (word count)
     word_dict_sorted = sorted(word_dict.items(), key=lambda x: x[-1])
+    # prints last 20 tuples in word_dict_sorted list
     [print(i[0], " : ", i[1]) for i in word_dict_sorted[:-21:-1]]
 
 
